aa8931c413d3a4bdeb2a9aadea9b9a93eb8feb0d
Komlavi Djossou <djokwa13@gmail.com>
2022-05-21 20:53:51 +0000

    w04

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
-require("dotenv").config() 
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+const cors = require('cors');
+
+const swaggerAutogen = require('swagger-autogen')();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+const port = process.env.PORT || 5000;
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'));
+app
+.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+.use(cors())
+.use(express.json())
+.use(bodyParser.json())
+.use(express.urlencoded({ extended: true}))
+.use('/', require('./routes'));
 
 
-app.listen(port, () => {
-    console.log(`Running on part ${port}`)
-})
+// const db = requre('./models');
+//   db.mongoose
+//   .connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then
 
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
 
